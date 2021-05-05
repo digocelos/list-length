48f625431507e7f3f485fa5c63b77dd1af27872e
rodrigo.vasconcelos <rodrigo.vasconcelos@siagri.com.br>
2021-05-05 11:09:37 -0300

    Create listlenght

--- lib/list_lenght.ex
+++ lib/list_lenght.ex
@@ -1,5 +1,11 @@
 defmodule ListLenght do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  defp count([], inc) do
+    inc
+  end
+
+  defp count([h | t], inc) do
+    count(t, inc + 1)
   end
 end
